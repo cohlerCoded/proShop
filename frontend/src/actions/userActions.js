a26497d85afcbeb4401236cfa3e4d9650820e7b9
cohlerCoded <jeffcohler@cohlercoded.com>
2021-06-10 12:40:55 -0700

    logout action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -36,3 +36,8 @@ export const login = (email, password) => async (dispatch) => {
     })
   }
 }
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userinfo')
+  dispatch({ type: USER_LOGOUT })
+}
