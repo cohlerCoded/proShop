566d6dcd062dd060048c05d2f1eb55c3bdba385e
cohlerCoded <jeffcohler@cohlercoded.com>
2021-06-14 14:48:38 -0700

    CART_SAVE_SHIPPING_ADDRESS saveShippingAddress fix

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
@@ -30,5 +34,5 @@ export const saveShippingAddress = (data) => (dispatch) => {
     type: CART_SAVE_SHIPPING_ADDRESS,
     payload: data,
   })
-  localStorage.setItem('shippingAddress', JSON.stringify(getState(data)))
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
