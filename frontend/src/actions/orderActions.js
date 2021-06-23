c728e7f72bc846fcf633c483d2e6a89ed9343d3f
cohlerCoded <jeffcohler@cohlercoded.com>
2021-06-22 17:51:01 -0700

    bug fix

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -89,7 +89,7 @@ export const payOrder =
           Authorization: `Bearer ${userInfo.token}`,
         },
       }
-      const { data } = await axios.get(
+      const { data } = await axios.put(
         `/api/orders/${orderId}/pay`,
         paymentResult,
         config
