9cd1c4410c9220e1db1ca5baef1af6565e639c80
cohlerCoded <jeffcohler@cohlercoded.com>
2021-06-23 12:15:59 -0700

    user orders list action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -6,6 +6,9 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
   ORDER_PAY_FAIL,
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
@@ -42,6 +45,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
     })
   }
 }
+
 export const getOrderDetails = (id) => async (dispatch, getState) => {
   try {
     dispatch({
@@ -72,6 +76,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     })
   }
 }
+
 export const payOrder =
   (orderId, paymentResult) => async (dispatch, getState) => {
     try {
@@ -108,3 +113,34 @@ export const payOrder =
       })
     }
   }
+
+export const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+    const { data } = await axios.get(`/api/orders/myorders`, config)
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
