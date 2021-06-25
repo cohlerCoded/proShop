efe0c6e2f09ddf6039a6b742f64c90707b1a9d90
cohlerCoded <jeffcohler@cohlercoded.com>
2021-06-25 13:00:02 -0700

    typo fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -179,7 +179,7 @@ export const listUsers = () => async (dispatch, getState) => {
         Authorization: `Bearer ${userInfo.token}`,
       },
     }
-    const { data } = await axios.ger(`/api/users`, config)
+    const { data } = await axios.get(`/api/users`, config)
     dispatch({
       type: USER_LIST_SUCCESS,
       payload: data,
