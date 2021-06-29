d93a7f957dfeb89c49f02d7180e3e7a6b8da3875
cohlerCoded <jeffcohler@cohlercoded.com>
2021-06-29 12:36:00 -0700

    create and update product controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,6 +22,52 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'sample',
+    price: 0,
+    user: req.user._id,
+    image: '/iamges/sample.jpg',
+    brand: 'sample',
+    category: 'sample',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'sample',
+  })
+
+  const createdProduct = await product.save()
+  res.sendStatus(201).json(createdProduct)
+})
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, image, category, countInStock, brand, description } =
+    req.body
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    product.name = name
+    product.price = price
+    product.image = image
+    product.category = category
+    product.countInStock = countInStock
+    product.brand = brand
+    product.description = description
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found')
+  }
+  const createdProduct = await product.save()
+  res.sendStatus(201).json(product)
+})
+
 // @desc Delete a product
 // @route DELETE /api/products
 // @access Private/Admin
