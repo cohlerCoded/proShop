05c2bd7978ab1380a0d8182b5b1c2a6a868504a4
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-03 13:56:53 -0700

    typos and other fixes

--- frontend/src/screens/OrderListScreen.jsx
+++ frontend/src/screens/OrderListScreen.jsx
@@ -35,6 +35,7 @@ const OrderListScreen = ({ history }) => {
           <thead>
             <tr>
               <th>ID</th>
+              <th>USER</th>
               <th>DATE</th>
               <th>TOTAL</th>
               <th>PAID</th>
@@ -44,10 +45,11 @@ const OrderListScreen = ({ history }) => {
           </thead>
           <tbody>
             {orders.map((order) => (
-              <tr hey={order._id}>
+              <tr key={order._id}>
                 <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
                 <td>{order.createdAt.substring(0, 10)}</td>
-                <td>{order.totalPrice}</td>
+                <td>${order.totalPrice}</td>
                 <td>
                   {order.isPaid ? (
                     order.paidAt.substring(0, 10)
@@ -63,7 +65,7 @@ const OrderListScreen = ({ history }) => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`order/${order._id}`}>
+                  <LinkContainer to={`/order/${order._id}`}>
                     <Button variant='light' className='btn-sm'>
                       Details
                     </Button>
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -123,10 +123,10 @@ const ProfileScreen = ({ location, history }) => {
             </thead>
             <tbody>
               {orders.map((order) => (
-                <tr hey={order._id}>
+                <tr key={order._id}>
                   <td>{order._id}</td>
                   <td>{order.createdAt.substring(0, 10)}</td>
-                  <td>{order.totalPrice}</td>
+                  <td>${order.totalPrice}</td>
                   <td>
                     {order.isPaid ? (
                       order.paidAt.substring(0, 10)
