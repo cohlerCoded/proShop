66858c068c4daded9208191a893548301cdd7be6
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-03 13:10:53 -0700

    get orders backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -73,13 +73,28 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc Get loggfed in user's orders
+// @desc Get logged in user's orders
 // @route GET /api/orders/myorders
 // @access Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({user: req.user._id})
-   res.json(orders)
+  const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
+// @desc Get all orders
+// @route GET /api/orders
+// @access Private
+
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+}
